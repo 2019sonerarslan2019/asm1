174894d026c1d23405f435c5ccd46b084bc1638f
Gokhan Suzen <gokhansuzen@yahoo.com>
2019-07-29 16:01:36 +0300

    v1.0

--- myapp/utils.py
+++ myapp/utils.py
@@ -3,18 +3,12 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
-
-
-
-
 def send_mail(subject, template_name, context, to):
     html_content = render_to_string(template_name, context)
     text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER,['gokhansuzen@yahoo.com'])
+    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER,[''])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
-
-
 
 
 
